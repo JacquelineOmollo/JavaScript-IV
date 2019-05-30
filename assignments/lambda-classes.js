8fd29a6e3ee21d58fc8c2329a691ed40ac03c00a
Jacqueline <jacquelineomollo@gmail.com>
2019-05-30 09:08:52 -0400

    Start of classes project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,10 @@
-// CODE here for your Lambda Classes
+class Person {
+  constructor(name, age, location) {
+    this.name = name;
+    this.age = age;
+    this.location = location;
+  }
+  speak() {
+    return `hello my name is ${name}, I am from ${location}.`;
+  }
+}
