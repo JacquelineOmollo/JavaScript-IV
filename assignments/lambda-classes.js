ef4e921a6da0d909a33a32c19371be6163c8c3b9
Jacqueline <jacquelineomollo@gmail.com>
2019-05-30 13:13:09 -0400

    Added some changes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,3 +8,26 @@ class Person {
     return `hello my name is ${name}, I am from ${location}.`;
   }
 }
+
+class Instructor {
+  constructor(attributes) {
+    this.specialty = attributes.specialty;
+    this.subject = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+  demo() {
+    return `Today we are learning about ${subject}.`;
+  }
+  grade() {
+    return `${student.name} receives a perfect score on ${subject}`;
+  }
+}
+
+class Student extends Person {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  }
+}
