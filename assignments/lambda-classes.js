58fbea108631e341e7814d3a2dc20527655bf124
Jacqueline <jacquelineomollo@gmail.com>
2019-05-30 22:19:28 -0400

    classes.js done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,7 +5,7 @@ class Person {
     this.location = attributes.location;
   }
   speak() {
-    return `hello my name is ${this.name}, I am from ${this.location}.`;
+    return `Hello my name is ${this.name}, I am from ${this.location}.`;
   }
 }
 
@@ -16,11 +16,11 @@ class Instructor extends Person {
     this.catchPhrase = instructorAttributes.catchPhrase;
     this.favLanguage = instructorAttributes.favLanguage;
   }
-  demo() {
+  demo(subject) {
     return `Today we are learning about ${subject}.`;
   }
   grade(student, subject) {
-    return `${student.name} receives a perfect score on ${subject}`;
+    return `${student} receives a perfect score on ${subject}.`;
   }
 }
 
@@ -38,17 +38,71 @@ class Student extends Instructor {
     }. `;
   }
 
-  PRAssignment() {
-    return `${student.name} has submitted a PR for {subject}`;
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
   }
 
-  sprintChallenge() {
-    return `${student.name} has begun sprint challenge on {subject}.`;
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}.`;
   }
 }
+
+class ProjectManager extends Student {
+  constructor(pmAttributes) {
+    super(pmAttributes);
+    this.gradClassName = pmAttributes.gradClassName;
+    this.favInstructor = pmAttributes.favInstructor;
+  }
+
+  standUp(channel) {
+    return `${this.name} announces to ${channel} @channel standy times!​​​​​`;
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student}'s code on ${subject}`;
+  }
+}
+
+const tom = new Person({
+  name: "Tom",
+  location: "North Carolina",
+  age: 50
+});
 
 const mary = new Instructor({
   name: "Mary",
   location: "New York",
-  age: 40
+  age: 80,
+  specialty: "Full-Stack Web",
+  catchPhrase: "Let build something.",
+  favLanguage: "Python"
 });
+
+const jackie = new Student({
+  name: "Jackie",
+  location: "Kenya",
+  age: 40,
+  previousBackground: "Dentist",
+  className: "WebPT7",
+  favSubjects: "Classes in JavaScript"
+});
+
+const eden = new ProjectManager({
+  name: "Eden",
+  location: "Brazil",
+  age: 20,
+  gradClassName: "Web102",
+  favInstructor: "Brad Traversy"
+});
+
+console.log(jackie.name);
+console.log(jackie.className);
+console.log(mary.favLanguage);
+console.log(jackie.listsSubjects());
+console.log(mary.demo("Node.js"));
+console.log(jackie.grade("Jackie", "JavaScript"));
+console.log(eden.standUp("Web102"));
+console.log(eden.debugsCode("Jackie", "Java"));
+console.log(jackie.speak());
+console.log(tom.speak());
+console.log(jackie.PRAssignment("classes"));
+console.log(jackie.sprintChallenge("Javascript"));
